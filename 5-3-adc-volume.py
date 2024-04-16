17f1d45587748bab1fd1b3e730e12c7894c65e8f
DanFiz <titarenko.di@phystech.edu>
2024-04-16 18:31:57 +0300

    lesson6

--- 5-3-adc-volume.py
+++ 5-3-adc-volume.py
@@ -1,44 +1,55 @@
-import RPi.GPIO as gpio
-from time import sleep
-gpio.setmode(gpio.BCM)
-dac = [8, 11, 7, 1, 0, 5, 12,6]
-comp = 14
-troyka = 13
-leds = [9, 10, 22,27, 17,4, 3, 2]
-gpio.setup(dac, gpio.OUT)
-gpio.setup(leds, gpio.OUT)
-gpio.setup(troyka,gpio.OUT, initial=gpio.HIGH)
-gpio.setup(comp, gpio.IN)
+import RPi.GPIO as GPIO
+import time
 
-def perev(a):
-    return [int (elem) for elem in bin(a)[2:].zfill(8)]
+def dec2bin(x):
+    return [int(i) for i in bin(x)[2:].zfill(8)]
 
 def adc():
-    k=0
-    for i in range(7, -1, -1):
-        k+=2**i
-        gpio.output(dac, perev(k))
-        sleep(0.005)
-        if gpio.input(comp)==gpio.HIGH:
-            k-=2**i
-    return k
+    value = 0
+    signals = [0] * 8
+    for i in range(8):
+        signals[i] = 1
+        GPIO.output(dac, signals)
+        time.sleep(0.001)
+        if GPIO.input(comp) == 1:
+            signals[i] = 0
+        else:
+            value += 2**(7-i)
+    return value
 
+GPIO.setmode(GPIO.BCM)
 
-def volume(n):
-    n=int(round(n/32,0))
-    mas=[0]*8
-    for i in range(n):
-        mas[i]=1
-    return mas
+dac = [8, 11, 7, 1, 0, 5, 12, 6]
+leds = [2, 3, 4, 17, 27, 22, 10, 9]
+comp = 14
+troyka = 13
+light = [0] * 8
+
+GPIO.setup(dac, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(troyka, GPIO.OUT, initial = GPIO.HIGH)
+GPIO.setup(leds, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(comp, GPIO.IN)
 
 try:
     while True:
-        k=adc()
-        if k!=0:
-            gpio.output(leds, volume(k))
-            print(int(k)/256*10)
+        val = adc()
+        print("V =", round(int(val) / 255 * 330)/100, "В", end = " ")
+        for x in dec2bin(val):
+            print(x, end = "")
+        print()
+        x = round(val/256*8)
+        for i in range(1,9):
+            if (i-0.5) <= x:
+                light[-i] = 1
+            else:
+                light[-i] = 0
+        GPIO.output(leds, light)
 
         
+except KeyboardInterrupt:
+    time.sleep(0)
 finally:
-    gpio.output(dac, 0)
-    gpio.cleanup()
+    GPIO.output(dac, 0)
+    GPIO.output(troyka, 0)
+    GPIO.output(leds, 0)
+    GPIO.cleanup()
